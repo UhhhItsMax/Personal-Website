75ca5538e8435a15be9eac0da855920980580e94
UhhhItsMax <69507108+UhhhItsMax@users.noreply.github.com>
2021-09-01 20:08:19 +0200

    Add files via upload

---
+++ mainpage/js/button.js
@@ -0,0 +1,12 @@
+/*var Btn = document.createElement.apply("button");
+Btn.id = "MyButton";
+Btn.innerHTML = "Invite EhrenBot";
+Btn.style.background = "#4FFF8F";
+document.body.appendChild(Btn);
+*/
+
+var clickMeButton = document.createElement('button');
+clickMeButton.id = 'myButton';
+clickMeButton.innerHTML = 'Click Me';
+clickMeButton.style.background = '#4FFF8F';
+document.body.appendChild(clickMeButton);
